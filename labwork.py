36216b132534dd9e4f982b1d1d7b2f158a6cf89b
Shaurav Zahan Mahmud <shauravzahan91@gmail.com>
2024-04-29 22:46:53 +0600

    Update labwork.py

--- labwork.py
+++ labwork.py
@@ -7,8 +7,9 @@ def main():
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
     if uploaded_file is not None:
-        file_bytes = uploaded_file.getvalue()
-        image = cv2.imdecode(np.frombuffer(file_bytes, np.uint8), -1)
+        file_bytes = uploaded_file.read()
+        nparr = np.frombuffer(file_bytes, np.uint8)
+        image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
         st.image(image, caption="Uploaded Image", use_column_width=True)
 
 if __name__ == "__main__":
